176bb22cc0f04fda9b783e414d8591a56336bde4
Schmidt <38161639+amschmidt2@users.noreply.github.com>
2021-10-06 17:49:30 -0500

    system print line

--- Preseaon_template/src/main/java/frc/robot/commands/ShootBall.java
+++ Preseaon_template/src/main/java/frc/robot/commands/ShootBall.java
@@ -20,7 +20,10 @@ public class ShootBall extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    System.out.println(Constants.SHOOTER_SPEED);
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
